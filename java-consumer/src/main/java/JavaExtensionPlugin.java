69a51eb721b0e01e830d6294315eb81ce0c1bf5b
Adam Murdoch <adam@gradle.com>
2020-02-25 12:12:53 +1100

    Add some more usages.

--- java-consumer/src/main/java/JavaExtensionPlugin.java
+++ java-consumer/src/main/java/JavaExtensionPlugin.java
@@ -1,11 +1,34 @@
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.sample.api.Providers;
+import org.gradle.sample.api.ValueProvider;
 import org.gradle.sample.plugins.JavaApiModel;
 import org.gradle.sample.plugins.KotlinApiModel;
+import org.jetbrains.annotations.Nullable;
 
 public class JavaExtensionPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
+        //
+        // Direct usage in Java
+        //
+
+        Providers providers = new Providers();
+
+        ValueProvider<String> a = providers.provider(String.class, "abc");
+        ValueProvider<@Nullable String> b = providers.nullableProvider(String.class);
+
+        // OK: no warnings or errors
+        int a1 = a.get().length();
+
+        // OK: Warns about potential NPE
+        int b1 = b.get().length();
+
+        // OK: no warnings or errors
+        int a2 = a.map(s -> 123).get();
+
+        // BROKEN: no warnings or errors
+        int a3 = a.map(s -> someInteger()).get();
 
         //
         // API defined in Java
@@ -30,5 +53,9 @@ public class JavaExtensionPlugin implements Plugin<Project> {
 
         // BROKEN: Does not warn about potential NPE
         int kotlin2 = kotlinApi.getNullableProp().get().length();
+    }
+
+    @Nullable Integer someInteger() {
+        return 123;
     }
 }
